0caaba648e96761643e53744a1e293687253ca63
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-03-02 20:28:20 -0300

    Extraindo o Dashboard Stless

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,60 @@ class BytebankApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.red[800],
-        accentColor: Colors.cyan[600],
+        //accentColor: Colors.cyan[600],
         fontFamily: 'RobotoMono',
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.red[800],
+          textTheme: ButtonTextTheme.primary
+        ),
         textTheme: TextTheme(
           headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
           headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
           bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
         )
       ),
-        home: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.wb_auto),
-            title: Text('The Avengers'),
-          ),
-
-          body: Column(
-            children: <Widget>[
-              Image.asset('images/marvel.png'),
-              Container(
-                height: 120,
-                width: 100,
-                color: Colors.red[800],
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.people),
-                    Text('Heroes')
-                  ],
-                ),
-              )
-            ],
-          ),
-    ));
+        home: Dashboard(),
+    );
   }
 }
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.wb_auto),
+        title: Text('The Avengers'),
+      ),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/marvel.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Theme.of(context).primaryColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.people,size: 32.0,),
+                  Text('Heroes')
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
