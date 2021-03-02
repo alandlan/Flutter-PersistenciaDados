1316bb82ec58e90d2bf705e1f9589681e3ca98c5
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-03-02 20:12:35 -0300

    Editado Tema

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,30 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.red[800],
+        accentColor: Colors.cyan[600],
+        fontFamily: 'RobotoMono',
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        )
+      ),
         home: Scaffold(
           appBar: AppBar(
-          title: Text('Dashboard'),
-      ),
+            leading: Icon(Icons.wb_auto),
+            title: Text('The Avengers'),
+          ),
+
           body: Column(
             children: <Widget>[
               Image.asset('images/marvel.png'),
               Container(
                 height: 120,
                 width: 100,
-                color: Colors.grey,
+                color: Colors.red[800],
                 child: Column(
                   children: <Widget>[
                     Icon(Icons.people),
