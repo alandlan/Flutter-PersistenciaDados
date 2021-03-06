d3d4ba1be4db0082db899c3640c9b43ee8ef1dac
Alan Vieira Martins <alanhorcrux@gmail.com>
2021-03-06 13:34:13 -0300

    adicionado async await

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,6 +1,67 @@
 import 'package:FlutterPersistenciaDados/models/caracter.dart';
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+
+// Future<Database> createDatabase() {
+//   return getDatabasesPath().then((dbPath) {
+//     final String path = join(dbPath, 'marvel.db');
+//     return openDatabase(
+//       path,
+//       onCreate: (db, version) {
+//         db.execute('CREATE TABLE caracter ('
+//             'id INTEGER PRIMARY KEY, '
+//             'name TEXT, '
+//             'slogan TEXT, '
+//             'born INTEGER, '
+//             'size INTEGER)');
+//       },
+//       version: 1,
+//       onDowngrade: onDatabaseDowngradeDelete,
+//     );
+//   });
+// }
+//
+// Future<int> save(Caracter caracter) {
+//   return createDatabase().then((db) {
+//     final Map<String, dynamic> caracterMap = Map();
+//     //caracterMap['id'] = caracter.id;
+//     caracterMap['name'] = caracter.name;
+//     caracterMap['slogan'] = caracter.slogan;
+//     caracterMap['born'] = caracter.born;
+//     caracterMap['size'] = caracter.size;
+//     return db.insert('caracter', caracterMap);
+//   });
+// }
+//
+// Future<List<Caracter>> findAll() {
+//   return createDatabase().then((db) {
+//     return db.query('caracter').then((maps) {
+//       final List<Caracter> caracters = List();
+//       for (Map<String, dynamic> map in maps) {
+//         final Caracter caracter = Caracter(
+//             map['id'], map['name'], map['slogan'], map['born'], map['size']);
+//         caracters.add(caracter);
+//       }
+//       return caracters;
+//     });
+//   });
+// }
+//
+// Future<void> removeAll() async {
+//   var databasesPath = await getDatabasesPath();
+//   String path = join(databasesPath, 'marvel.db');
+//
+//   Database database = await openDatabase(
+//     path,
+//     version: 1,
+//     onOpen: (db) {
+//       db.rawQuery('DELETE FROM caracter');
+//     },
+//   );
+//
+//   await database.close();
+// }
 
 Future<Database> getDatabase() async {
   final String path = join(await getDatabasesPath(), 'marvel.db');
@@ -17,23 +78,6 @@ Future<Database> getDatabase() async {
     version: 1,
     onDowngrade: onDatabaseDowngradeDelete,
   );
-
-  // return getDatabasesPath().then((dbPath) {
-  //   final String path = join(dbPath, 'marvel.db');
-  //   return openDatabase(
-  //     path,
-  //     onCreate: (db, version) {
-  //       db.execute('CREATE TABLE caracter ('
-  //           'id INTEGER PRIMARY KEY, '
-  //           'name TEXT, '
-  //           'slogan TEXT, '
-  //           'born INTEGER, '
-  //           'size INTEGER)');
-  //     },
-  //     version: 1,
-  //     onDowngrade: onDatabaseDowngradeDelete,
-  //   );
-  // });
 }
 
 Future<int> save(Caracter caracter) async {
@@ -44,16 +88,6 @@ Future<int> save(Caracter caracter) async {
   caracterMap['born'] = caracter.born;
   caracterMap['size'] = caracter.size;
   return db.insert('caracter', caracterMap);
-
-  // return createDatabase().then((db) {
-  //   final Map<String, dynamic> caracterMap = Map();
-  //   //caracterMap['id'] = caracter.id;
-  //   caracterMap['name'] = caracter.name;
-  //   caracterMap['slogan'] = caracter.slogan;
-  //   caracterMap['born'] = caracter.born;
-  //   caracterMap['size'] = caracter.size;
-  //   return db.insert('caracter', caracterMap);
-  // });
 }
 
 Future<List<Caracter>> findAll() async {
@@ -63,22 +97,15 @@ Future<List<Caracter>> findAll() async {
 
   for (Map<String, dynamic> row in result) {
     final Caracter caracter = Caracter(
-        row['id'], row['name'], row['slogan'], row['born'], row['size']);
+      row['id'],
+      row['name'],
+      row['slogan'],
+      row['born'],
+      row['size'],
+    );
     caracters.add(caracter);
   }
   return caracters;
-
-  // return createDatabase().then((db) {
-  //   return db.query('caracter').then((maps) {
-  //     final List<Caracter> caracters = List();
-  //     for (Map<String, dynamic> map in maps) {
-  //       final Caracter caracter = Caracter(
-  //           map['id'], map['name'], map['slogan'], map['born'], map['size']);
-  //       caracters.add(caracter);
-  //     }
-  //     return caracters;
-  //   });
-  // });
 }
 
 Future<void> removeAll() async {
